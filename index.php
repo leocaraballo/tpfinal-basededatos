2deaee7a3f9dc3b166e182a79251631a665f0fc4
Leo Caraballo <leocaraqq@gmail.com>
2018-11-30 02:51:42 -0300

    Cambio

--- index.php
+++ index.php
@@ -32,8 +32,8 @@
 	<div class="w3-bar w3-blue w3-card w3-left-align w3-large">
 		<a class="w3-bar-item w3-button w3-hide-medium w3-hide-large w3-right w3-padding-large w3-hover-white w3-large w3-red" href="javascript:void(0);" onclick="myFunction()" title="Toggle Navigation Menu"><i class="fa fa-bars"></i></a>
 		<a class="w3-bar-item w3-button w3-padding-large w3-white">Inicio</a>
-		<a href="supercaro/producto" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">producto</a>
-		<a href="supercaro/Conozcanos/" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">inventario</a>
+		<a href="supercaro/producto" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Producto</a>
+		<a href="supercaro/Conozcanos/" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Inventario</a>
 		<a href="supercaro/App" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Lote</a>
 		<a onclick="document.getElementById('id02').style.display='block'" href="#" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Verificaciones</a>
 	</div>
@@ -42,8 +42,8 @@
 	<div id="navDemo" class="w3-bar-block w3-white w3-hide w3-hide-large w3-hide-medium w3-large">
 		<a class="w3-bar-item w3-button w3-padding-large w3-white">Inicio</a>
 		<a href="#" class="w3-bar-item w3-button w3-padding-large">Producto</a>
-		<a href="/supercaro/Conozcanos" class="w3-bar-item w3-button w3-padding-large">Conozcanos</a>
-		<a onclick="document.getElementById('id02').style.display='block'" href="#" class="w3-bar-item w3-button w3-padding-large">Contactenos</a>
+		<a href="/supercaro/Conozcanos" class="w3-bar-item w3-button w3-padding-large">Lote</a>
+		<a onclick="document.getElementById('id02').style.display='block'" href="#" class="w3-bar-item w3-button w3-padding-large">Verificaciones</a>
 	</div>
 </div>
 
