649aca6e60fc26a79aaecaadd2431477078239a8
Wsott <theguiltyspark343@outlook.com>
2018-11-30 14:08:44 -0300

    Update header.php

--- vistas/templates/header.php
+++ vistas/templates/header.php
@@ -73,7 +73,7 @@
       <div class="w3-bar w3-blue w3-card w3-left-align w3-large">
         <a class="w3-bar-item w3-button w3-hide-medium w3-hide-large w3-right w3-padding-large w3-hover-white w3-large w3-red"
           href="javascript:void(0);" onclick="myFunction()" title="Toggle Navigation Menu"><i class="fa fa-bars"></i></a>
-        <a href="/tpfinal-basededatos/" class="w3-bar-item w3-button w3-padding-large w3-white">Inicio</a>
+        <a href="/tpfinal-basededatos/" class="w3-bar-item w3-button w3-padding-large w3-hover-white">Inicio</a>
         <a href="/tpfinal-basededatos/controladores/producto.php" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Producto</a>
         <a href="/tpfinal-basededatos/" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Inventario</a>
         <a href="/tpfinal-basededatos/controladores/lotes.php" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Lote</a>
