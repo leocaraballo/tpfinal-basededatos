f1c35a669c0ced52907ba38492ad53390f9ab013
ramirezleonel <ramirezleonel0@gmail.com>
2018-11-30 02:03:27 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,18 +1,3 @@
-<?php 
-require('modelos/Conexion.inc.php');
-Conexion::openConnection();
-
-//if (Conexion::getConnection() != null) {
-	# code...
-	?>
-	<h1>Conectado!.</h1>
-	<?php 
-//}else{
-	
-	//<h1>Error de Conexion!.</h1>
-	//<?php
- //} 
- ?>
 <!DOCTYPE html>
 <html lang="en">
 <title>SuperCaro</title>
@@ -37,9 +22,9 @@ body,h1,h2,h3,h4,h5,h6 {font-family: "Lato", sans-serif}
 	<div class="w3-bar w3-blue w3-card w3-left-align w3-large">
 		<a class="w3-bar-item w3-button w3-hide-medium w3-hide-large w3-right w3-padding-large w3-hover-white w3-large w3-red" href="javascript:void(0);" onclick="myFunction()" title="Toggle Navigation Menu"><i class="fa fa-bars"></i></a>
 		<a class="w3-bar-item w3-button w3-padding-large w3-white">Inicio</a>
-		<a href="tpfinal-basededatos/producto" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">producto</a>
-		<a href="tpfinal-basededatos/inventario" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">inventario</a>
-		<a href="tpfinal-basededatos/lote" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Lote</a>
+		<a href="supercaro/producto" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">producto</a>
+		<a href="supercaro/Conozcanos/" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">inventario</a>
+		<a href="supercaro/App" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Lote</a>
 		<a onclick="document.getElementById('id02').style.display='block'" href="#" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Verificaciones</a>
 	</div>
 
@@ -53,7 +38,7 @@ body,h1,h2,h3,h4,h5,h6 {font-family: "Lato", sans-serif}
 </div>
 
 <!-- Header -->
-<header 	class="w3-container w3-center" style="padding:128px 16px;  background:url(img/almacen.jpg) no-repeat";>
+<header class="w3-container w3-light-blue w3-center" style="padding:128px 16px">
 	
 		<h1  class="w3-margin w3-jumbo"><strong>SUPERCARO</strong></h1> 
 	<!-- <p class="w3-xlarge">Template by w3.css</p> -->
@@ -65,7 +50,7 @@ body,h1,h2,h3,h4,h5,h6 {font-family: "Lato", sans-serif}
 		<h5>Stanisław Jerzy Lec</h5>
 </div>
 
-<!-- Footer -->
+<-- Footer -->
 <footer class="w3-container w3-teal w3-center w3-margin-top">  
 	<div class="w3-xlarge w3-padding-32">
 		<a href="https://www.facebook.com" style="text-decoration:none;" class="fab fa-facebook w3-hover-opacity" href="https://facebook.com" target="_blank"></a>
@@ -84,9 +69,98 @@ body,h1,h2,h3,h4,h5,h6 {font-family: "Lato", sans-serif}
 	
 				<div class="w3-center"><br>
 						<span onclick="document.getElementById('id01').style.display='none'" class="w3-button w3-xlarge w3-transparent w3-display-topright" title="Close Modal">×</span>
-						
+						<img src="Logo.png" alt="Avatar" style="width:25%" class="w3-circle w3-margin-top">
 				</div>
 
-				<form class="w3-container" action="/tpfinal-basededatos">
+				<form class="w3-container" action="/supercaro">
 						<div class="w3-section">
-								<label><
+								<label><b>Usuario</b></label>
+								<input class="w3-input w3-border w3-margin-bottom" type="text" placeholder="Ingrese su usuario" name="Usuario" required>
+								<label><b>Contraseña</b></label>
+								<input class="w3-input w3-border" type="password" placeholder="Ingrese su contraseña" name="Pass" required>
+								<button class="w3-button w3-block w3-green w3-section w3-padding" type="submit">Iniciar sesion</button>
+						</div>
+				</form>
+
+				<div class="w3-container w3-border-top w3-padding-16 w3-light-grey">
+						<button onclick="document.getElementById('id01').style.display='none'" type="button" class="w3-button w3-red">Cancelar</button>
+						<span class="w3-right w3-padding w3-hide-small"> <a href="#">¿Olvido su contraseña?</a></span>
+				</div>
+
+				</div>
+		</div>
+</div>
+
+
+<div id="id02" class="w3-modal">
+				<div class="w3-container w3-card-4 w3-light-grey w3-text-blue w3-modal-content w3-card-4 w3-animate-zoom" style="max-width:600px">
+						<span onclick="document.getElementById('id02').style.display='none'" class="w3-button w3-xlarge w3-transparent w3-display-topright" title="Close Modal">×</span>
+
+						<h2 class="w3-center">Verificacion</h2>
+ 
+						<div class="w3-row w3-section">
+							<div class="w3-col" style="width:50px"><i class="w3-xxlarge fa fa-user"></i></div>
+								<div class="w3-rest">
+									<input class="w3-input w3-border" name="first" type="text" placeholder="Nombre">
+								</div>
+						</div>
+						
+						<div class="w3-row w3-section">
+							<div class="w3-col" style="width:50px"><i class="w3-xxlarge fa fa-user"></i></div>
+								<div class="w3-rest">
+									<input class="w3-input w3-border" name="last" type="text" placeholder="Apellido">
+								</div>
+						</div>
+						
+						<div class="w3-row w3-section">
+							<div class="w3-col" style="width:50px"><i class="w3-xxlarge fa fa-envelope-o"></i></div>
+								<div class="w3-rest">
+									<input class="w3-input w3-border" name="email" type="text" placeholder="Correo">
+								</div>
+						</div>
+						
+						<div class="w3-row w3-section">
+							<div class="w3-col" style="width:50px"><i class="w3-xxlarge fa fa-phone"></i></div>
+								<div class="w3-rest">
+									<input class="w3-input w3-border" name="phone" type="text" placeholder="Telefono (Opcional)">
+								</div>
+						</div>
+						
+						<div class="w3-row w3-section">
+							<div class="w3-col" style="width:50px"><i class="w3-xxlarge fa fa-pencil"></i></div>
+								<div class="w3-rest">
+									<input class="w3-input w3-border" name="message" type="text" placeholder="Mensaje">
+								</div>
+						</div>
+						
+						<p class="w3-center">
+						<button class="w3-button w3-section w3-blue w3-ripple"> verificar </button>
+						</p>
+</div>
+
+
+
+
+
+
+
+
+
+<script>
+// Used to toggle the menu on small screens when clicking on the menu button
+function myFunction() {
+		var x = document.getElementById("navDemo");
+		if (x.className.indexOf("w3-show") == -1) {
+				x.className += " w3-show";
+		} else { 
+				x.className = x.className.replace(" w3-show", "");
+		}
+}
+
+   if(typeof window.history.pushState == 'function') {
+        window.history.pushState({}, "Hide", "http://localhost/supercaro");
+    }
+</script>
+
+</body>
+</html>
