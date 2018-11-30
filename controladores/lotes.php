d86680131bd9d3fb911577e3b60e531ad3b42e6b
Leo Caraballo <leocaraqq@gmail.com>
2018-11-30 05:55:02 -0300

    Mensaje Lote exito

--- controladores/lotes.php
+++ controladores/lotes.php
@@ -1,4 +1,5 @@
 <?php
+  require($_SERVER['DOCUMENT_ROOT'].'/tpfinal-basededatos/controladores/mostrar.php');
   if (isset($_REQUEST["agregar"])) {
     $data = [
       ":Producto_Codigo_FK" => $_REQUEST["producto"],
@@ -8,8 +9,8 @@
       ":Cantidad" => $_REQUEST["cantidad"],
     ];
     Lote::crear($data);
+    mostrar($_SERVER['DOCUMENT_ROOT'].'/tpfinal-basededatos/vistas/lotes_exito.php');
   } else {
-    require($_SERVER['DOCUMENT_ROOT'].'/tpfinal-basededatos/controladores/mostrar.php');
     require($_SERVER['DOCUMENT_ROOT'].'/tpfinal-basededatos/modelos/Producto.php');
     mostrar($_SERVER['DOCUMENT_ROOT'].'/tpfinal-basededatos/vistas/lotes.php');
   }
---
+++ vistas/lotes_exito.php
@@ -0,0 +1,3 @@
+<main class="w3-container" style="padding:128px 16px;">
+  <h1>Se ha agregado un lote exitosamente!</h1>
+</main>
