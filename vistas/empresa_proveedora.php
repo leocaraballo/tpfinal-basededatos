550f8950ed42d3092011bc1a3a7eeb25f5673e25
Wsott <theguiltyspark343@outlook.com>
2018-11-30 12:55:48 -0300

    Update empresa_proveedora.php

--- vistas/empresa_proveedora.php
+++ vistas/empresa_proveedora.php
@@ -42,7 +42,9 @@ $empresas = EmpresaProveedora::retornarTodasEmpresa_Proveedora(Conexion::getConn
                     '<td>'. $value->direccion .'</td>' .
                     '<td>'. $value->telefono .'</td>' .
                     '<td>'. $value->email .'</td>'.
-                    '<td><a href="empresa_proveedora.php?rne=' . $value->rne . '&accion=M">Modificar</a></td>'.
+                    '<td><a href="empresa_proveedora.php?rne=' . $value->rne . '&cuit=' . $value->cuit . '&nombre=' . $value->nombre . 
+                                                      '&direccion=' . $value->direccion . '&telefono=' . $value->telefono . '&email=' . $value->email . 
+                                                      '&accion=M">Modificar</a></td>'.
                     '</tr>';
         }
         ?>
@@ -52,39 +54,61 @@ $empresas = EmpresaProveedora::retornarTodasEmpresa_Proveedora(Conexion::getConn
     <form class="w3-container" style="text-align:left;" action="empresa_proveedora.php">
           <div class="w3-section">
             <?php
-                    if (isset($_GET['rne']) && isset($_GET['accion'])) {
-                        $action = 'M';
-                        ?>
+              if (isset($_GET['rne']) && isset($_GET['accion'])) {
+                $action = 'M';
+            ?>
               
               <input class="w3-input w3-border w3-margin-bottom" type="text" placeholder="Ingrese la accion" name="accion" style="display:none" value=<?php echo $action;?> >
                 <label><b>RNE</b></label>
-                <input class="w3-input w3-border w3-margin-bottom" type="number" placeholder="Ingrese el RNE de la empresa" name="rne"
-                       value=<?php echo $_GET['rne'];?>>
-                        <?php
-                    }else{
-                        ?>
-                <input class="w3-input w3-border w3-margin-bottom" type="text" placeholder="Ingrese la accion" disabled name="accion" style="display:none" value=<?php echo $action;?>>
-                <label><b>RNE</b></label>
-                <input class="w3-input w3-border w3-margin-bottom" type="number" placeholder="Ingrese el RNE de la empresa" name="rne" value=""
-                  required>
-              <?php
-                    }
+                  <input class="w3-input w3-border w3-margin-bottom" type="number" placeholder="Ingrese el RNE de la empresa" name="rne"
+                    value=<?php echo $_GET['rne'];?>>
+
+                <label><b>CUIT</b></label>
+                  <input class="w3-input w3-border w3-margin-bottom" type="number" placeholder="Ingrese el CUIT sin guiones" name="cuit"
+                    value=<?php echo $_GET['cuit'];?>>
+
+                <label><b>Nombre</b></label>
+                  <input class="w3-input w3-border w3-margin-bottom" type="text" placeholder="Ingrese el nombre de la empresa" name="nombre"
+                    value=<?php echo $_GET['nombre'];?>>
+
+                <label><b>Direccion</b></label>
+                  <input class="w3-input w3-border w3-margin-bottom" type="text" placeholder="Ingrese la direccion de la empresa" name="direccion"
+                    value=<?php echo $_GET['direccion'];?>>
+
+                <label><b>Telefono</b></label>
+                  <input class="w3-input w3-border w3-margin-bottom" type="number" placeholder="Ingrese el telefono de la empresa" name="telefono"
+                    value=<?php echo $_GET['telefono'];?>>
+
+                <label><b>Email</b></label>
+                  <input class="w3-input w3-border w3-margin-bottom" type="text" placeholder="Ingrese el email de la empresa" name="email"
+                    value=<?php echo $_GET['email'];?>>
+            <?php
+              }else{
             ?>
-            <label><b>CUIT</b></label>
-            <input class="w3-input w3-border w3-margin-bottom" type="number" placeholder="Ingrese el CUIT sin guiones" name="cuit"
+              <input class="w3-input w3-border w3-margin-bottom" type="text" placeholder="Ingrese la accion" disabled name="accion" style="display:none" value=<?php echo $action;?>>
+              <label><b>RNE</b></label>
+              <input class="w3-input w3-border w3-margin-bottom" type="number" placeholder="Ingrese el RNE de la empresa" name="rne" value=""
+                  required>
+              <label><b>CUIT</b></label>
+                <input class="w3-input w3-border w3-margin-bottom" type="number" placeholder="Ingrese el CUIT sin guiones" name="cuit"
               required>
-            <label><b>Nombre</b></label>
-            <input class="w3-input w3-border w3-margin-bottom" type="text" placeholder="Ingrese el nombre de la empresa" name="nombre"
-              required>
-            <label><b>Direccion</b></label>
-            <input class="w3-input w3-border w3-margin-bottom" type="text" placeholder="Ingrese la direccion de la empresa" name="direccion"
-              required>
-            <label><b>Telefono</b></label>
-            <input class="w3-input w3-border w3-margin-bottom" type="number" placeholder="Ingrese el telefono de la empresa" name="telefono"
-              required>
-            <label><b>Email</b></label>
-            <input class="w3-input w3-border w3-margin-bottom" type="text" placeholder="Ingrese el email de la empresa" name="email"
-              required>
+              <label><b>Nombre</b></label>
+              <input class="w3-input w3-border w3-margin-bottom" type="text" placeholder="Ingrese el nombre de la empresa" name="nombre"
+                required>
+              <label><b>Direccion</b></label>
+              <input class="w3-input w3-border w3-margin-bottom" type="text" placeholder="Ingrese la direccion de la empresa" name="direccion"
+                required>
+              <label><b>Telefono</b></label>
+              <input class="w3-input w3-border w3-margin-bottom" type="number" placeholder="Ingrese el telefono de la empresa" name="telefono"
+                required>
+              <label><b>Email</b></label>
+              <input class="w3-input w3-border w3-margin-bottom" type="text" placeholder="Ingrese el email de la empresa" name="email"
+                required>
+            <?php
+              }
+            ?>
+            
+            
             <button class="w3-button w3-block w3-green w3-section w3-padding" type="submit" name="enviar">Agregar/Modificar Empresa</button>
           </div>
   </form>
