cb82ec360e44b33c4da20e1cb4adff262b4ad247
Wsott <theguiltyspark343@outlook.com>
2018-11-30 05:56:33 -0300

    Create TipoVerificacion.php

---
+++ modelos/TipoVerificacion.php
@@ -0,0 +1,87 @@
+<?php
+    class TipoVerificacion{
+        public $Nombre;
+        public $Tipo;
+
+        function __construct() {}
+
+        function Consultar($Nombre){
+            require($_SERVER['DOCUMENT_ROOT'].'/tpfinal/modelos/Conexion.inc.php');
+            Conexion::openConnection();
+
+            $Conexion = Conexion::getConnection();
+
+            $Operacion = ("SELECT * FROM tipo_verificacion WHERE Nombre = '$Nombre'");
+
+            $Resultados = $Conexion->query($Operacion);
+
+            foreach($Resultados as $Item){
+                $this->Nombre = $Item["Nombre"];
+                $this->Tipo = $Item["Tipo"];
+            }
+
+            return $this;
+        } 
+
+        function Crear($Nombre, $Tipo){
+            require($_SERVER['DOCUMENT_ROOT'].'/tpfinal/modelos/Conexion.inc.php');
+            Conexion::openConnection();
+
+            $Conexion = Conexion::getConnection();
+
+            $Operacion = (" INSERT INTO tipo_verificacion
+                            (`Nombre`,
+                            `Tipo`)
+                            VALUES
+                            ('$Nombre',
+                            '$Tipo');");
+
+            if($Conexion->query($Operacion)){
+                $this->Nombre = $Nombre;
+                $this->Tipo = $Tipo;
+
+                return $this;
+            }
+            else{
+                return null;
+            }
+        }
+
+        function Modificar($Nombre, $NuevoNombre, $Tipo){
+            require($_SERVER['DOCUMENT_ROOT'].'/tpfinal/modelos/Conexion.inc.php');
+            Conexion::openConnection();
+
+            $Conexion = Conexion::getConnection();
+
+            $Operacion = (" UPDATE tipo_verificacion
+                            SET
+                            `Nombre` = '$NuevoNombre',
+                            `Tipo` = '$Tipo'
+                            WHERE 
+                            `Nombre` = '$Nombre';");
+
+            $Conexion->query($Operacion);
+
+            $this->Nombre = $NuevoNombre;
+            $this->Tipo = $Tipo;
+
+            return $this;
+        }
+
+        function Borrar($Nombre){
+            require($_SERVER['DOCUMENT_ROOT'].'/tpfinal/modelos/Conexion.inc.php');
+            Conexion::openConnection();
+
+            $Conexion = Conexion::getConnection();
+
+            $Operacion = ("DELETE FROM tipo_verificacion WHERE Nombre = '$Nombre'");
+
+            if($Conexion->query($Operacion)){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+    }
+?>
