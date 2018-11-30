eb6d15abf0a54b9d11620b460666fef18fafc943
lgtLeonardo <43980857+lgtLeonardo@users.noreply.github.com>
2018-11-30 03:04:56 -0300

    Update Footer.inc.php

--- Plantillas/Footer.inc.php
+++ Plantillas/Footer.inc.php
@@ -1,5 +1,5 @@
 
-<-- Footer -->
+<!-- Footer -->
 <footer class="w3-container w3-teal w3-center w3-margin-top">  
 	<div class="w3-xlarge w3-padding-32">
 		<a href="https://www.facebook.com" style="text-decoration:none;" class="fab fa-facebook w3-hover-opacity" href="https://facebook.com" target="_blank"></a>
