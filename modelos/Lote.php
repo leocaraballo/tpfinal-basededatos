447473036e0bd4085ad53c45ad99563f9807a5d0
Leo Caraballo <leocaraqq@gmail.com>
2018-11-30 17:12:50 -0300

    getalllotes

--- modelos/Lote.php
+++ modelos/Lote.php
@@ -13,5 +13,21 @@
         Conexion::closeConnection();
       }
     }
+
+    public static function getAllLotes() {
+      require_once($_SERVER['DOCUMENT_ROOT'].'/tpfinal-basededatos/modelos/Conexion.inc.php');
+      Conexion::openConnection();
+      $db = Conexion::getConnection();
+      $ret = null;
+      if ($db != null) {
+        $sql = "SELECT L.Numero as Lote_Numero, P.Nombre as Producto_Nombre, P.Marca as Producto_Marca, 
+                       P.Empresa_Proveedora_RNE_FK as RNE, P.RNPA as Producto_RNPA, EP.Nombre as Proveedor_Nombre
+                FROM Lote L JOIN Producto P ON L.Producto_Codigo_FK = P.Codigo
+                            JOIN Empresa_Proveedora EM ON EM.RNE = P.Empresa_Proveedora_RNE_FK";
+        $ret = $db->query($sql);
+      }
+      Conexion::closeConnection();
+      return $ret;
+    }
   }
 ?>
