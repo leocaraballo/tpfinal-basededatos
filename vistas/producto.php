3f3b506ed3a9736d7643a7213146052480f68b5a
Wsott <theguiltyspark343@outlook.com>
2018-12-07 05:10:30 -0300

    Update producto.php

--- vistas/producto.php
+++ vistas/producto.php
@@ -1,5 +1,5 @@
 <main class="w3-container" style="padding:128px 16px;">
-<header class="w3-container w3-center w3-centered w3-card-4" style="padding:128px 16px;">
+<header class="w3-container w3-center w3-centered " style="padding:128px 16px;">
   <table class="w3-table w3-striped w3-table-all">
       <tr>
         <th>Codigo</th>
@@ -17,15 +17,17 @@
       ?>
   </table>
 </header>
-  <h1 class="white-l">Nuevo Producto</h1>
+</main>
+<div class="w3-container w3-light-grey">
+  <h1 class="">Nuevo Producto</h1>
   <form action="/tpfinal-basededatos/controladores/producto.php" method="post">
-    <label class="white-l"><b>Código</b></label>
+    <label class=""><b>Código</b></label>
     <input class="w3-input w3-border w3-margin-bottom" type="text" placeholder="Ingrese Codigo de producto" name="Codigo"
       required>
-    <label class="white-l"><b>Nombre</b></label>
+    <label class=""><b>Nombre</b></label>
     <input class="w3-input w3-border w3-margin-bottom" type="text" placeholder="Ingrese el nombre del producto" name="Nombre"
       required>
-    <label class="white-l"><b>Registro de Numero de Establecimiento</b></label>
+    <label class=""><b>Registro de Numero de Establecimiento</b></label>
     <select name="Empresa_Proveedora_RNE_FK" class="w3-input w3-border w3-margin-bottom" required>
       <option value="" selected disabled>Seleccione un RNE</option>
       <?php foreach (EmpresaProveedora::getEmpresasProveedoras() as $empresa): ?>
@@ -33,24 +35,24 @@
       <?php endforeach; ?>
     </select>
 
-    <label class="white-l"><b>Marco Regulatorio</b></label>
+    <label class=""><b>Marco Regulatorio</b></label>
     <select name="Marco_Regulatorio_Numero_FK" class="w3-input w3-border w3-margin-bottom">
       <option value="" selected disabled required>Seleccione un MR</option>
       <?php foreach (MarcoRegulatorio::getMarcosRegulatorios() as $mr): ?>
         <option value="<?=$mr["Numero"]?>">MR <?=$mr["Numero"]?></option>
       <?php endforeach; ?>
     </select>
-    <label class="white-l"><b>Registro Nacional Producto Alimenticio</b></label>
+    <label class=""><b>Registro Nacional Producto Alimenticio</b></label>
     <input class="w3-input w3-border w3-margin-bottom" type="text" placeholder="Ingrese el RNPA" name="RNPA" required>
 
-    <label class="white-l"><b>Descripción</b></label>
+    <label class=""><b>Descripción</b></label>
     <input class="w3-input w3-border w3-margin-bottom" type="text" placeholder="Ingrese la Descripcion" name="Descripcion"
       required>
 
-    <label class="white-l"><b>Marca</b></label>
+    <label class=""><b>Marca</b></label>
     <input class="w3-input w3-border w3-margin-bottom" type="text" placeholder="Ingrese la Marca" name="Marca" required>
 
-    <table class="white-l">
+    <table class="">
       <tr>
         <th>Categorias</th>
         <th></th>
@@ -67,4 +69,4 @@
     <button class="w3-button w3-block w3-green w3-section w3-padding" type="submit">Agregar</button>
     <button class="w3-button w3-block w3-yellow w3-section w3-padding ">Modificar</button>
   </form>
-</main>
+    </div>
